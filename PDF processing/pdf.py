0cc54ebd51e7ded65b15e9c3d555ca020e22e908
kunalbandale <bandalekunal@gmai.com>
2024-08-16 09:38:28 +0530

    pdf testing completed

--- PDF processing/pdf.py
+++ PDF processing/pdf.py
@@ -1,18 +1,32 @@
 import PyPDF2
+import sys
 
-with open('Resume.pdf' , 'rb') as file:
-    reader = PyPDF2.PdfFileReader(file)
-    # output number of pages
-    print(reader.numPages)
-    # page object
-    page = reader.getPage(0)
-    print(page)
-    print(page.rotateCounterClockwise(90))
-    writer = PyPDF2.PdfFileWriter()
-    writer.addPage(page)
+inputs = sys.argv[1:]
 
-    with open('new.pdf' , 'wb') as file:
-        writer.write(file)
+def pdf_merger(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('super.pdf')
+
+pdf_merger(inputs)
+
+
+# with open('Resume.pdf' , 'rb') as file:
+#     reader = PyPDF2.PdfFileReader(file)
+#     # output number of pages
+#     print(reader.numPages)
+#     # page object
+#     page = reader.getPage(0)
+#     print(page)
+#     print(page.rotateCounterClockwise(90))
+#     writer = PyPDF2.PdfFileWriter()
+#     writer.addPage(page)
+
+#     with open('new.pdf' , 'wb') as file:
+#         writer.write(file)
+
 
 
 
